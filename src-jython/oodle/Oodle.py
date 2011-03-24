fe08a93e9847d8f08cf7685d4628e5520c9ea9ee
Joseph Freeman <jfree143dev@gmail.com>
2011-03-23 22:29:14 -0400

    Forgot to specify the name of <ORGANIZATION> in the license...oops.

--- src-jython/oodle/Oodle.py
+++ src-jython/oodle/Oodle.py
@@ -1,4 +1,3 @@
-#Oodle.py
 #===============================================================================
 # Copyright (c) 2011, Joseph Freeman <jfree143dev AT gmail DOT com>
 # All rights reserved.
@@ -11,7 +10,7 @@
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-# * Neither the name of the <ORGANIZATION> nor the names of its contributors may
+# * Neither the name of Joseph Freeman nor the names of its contributors may
 #   be used to endorse or promote products derived from this software without
 #   specific prior written permission.
 # 
